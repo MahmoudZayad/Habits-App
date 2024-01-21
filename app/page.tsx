3ca82b05710650ee3938352c7f7e4f4fa40d69ac
MahmoudZayad <mahmoudzayad@gmail.com>
2024-01-20 20:06:52 -0600

    added habitRow

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import HabitCheck from '@/app/ui/habits/habit-check'
+import {HabitCheck} from '@/app/ui/habits/habit-check'
+import HabitRow from '@/app/ui/habits/habit-check';
 export default function HabitTable() {
   return (
       <div>
+          <HabitRow/>
           <HabitCheck completed = {true}/>
           <h1>HELLO</h1>
       </div>
--- app/ui/habits/habit-check.tsx
+++ app/ui/habits/habit-check.tsx
@@ -1,13 +1,36 @@
 'use client'
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import {
     CheckIcon,
     XMarkIcon,
 } from '@heroicons/react/24/outline'
 
-export default function HabitCheck({completed}:{completed:boolean}) {
+
+
+export default function HabitRow() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  
+    useEffect(() => {
+      const handleResize = () => setWindowWidth(window.innerWidth);
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    const numberOfChecks = Math.floor(windowWidth / 50); // Change 200 to the width of your buttons
+    const buttons = Array.from({ length: numberOfChecks }, (_, i) => i + 1);
+  
+    return (
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {buttons.map((buttonIndex) => (
+          <HabitCheck completed = {true} key={buttonIndex}/>
+        ))}
+      </div>
+    );
+  }
+
+export function HabitCheck({completed}:{completed:boolean}) {
 
     const [complete, setComplete] = useState(completed)
 
